10a69c6a0ceee64b7b97bfcb263e9a9bcbc11673
Daniel <clwnssla1@bible.ac.kr>
2023-03-21 00:24:55 +0900

    Feat : create chapter5 code.

--- 이메일 전송 자동화 프로젝트/emailSendFunction.py
+++ 이메일 전송 자동화 프로젝트/emailSendFunction.py
@@ -1,4 +1,5 @@
 import os
+import smtplib
 # 이메일 메시지에 다양한 형식을 중첩하여 담기 위한 객체
 from email.mime.multipart import MIMEMultipart
 
@@ -12,24 +13,33 @@ from email.mime.image import MIMEImage
 # 오디오 형식
 from email.mime.audio import MIMEAudio
 
+# 위의 모든 객체들을 생성할 수 있는 기본 객체
 # 위의 모든 객체를 생성할 수 있는 기본 객체
 # MIMEBase(_maintype, _subtype)
 # MIMEBase(<메인 타입>, <서브 타입>)
 from email.mime.base import MIMEBase
 
-msg_dict = {
-    'text' : {'maintype' : 'text', 'subtype' : 'plain', 'filename' : 'res/email_sending/test.txt'}, # 텍스트 첨부파일
-    'image' : {'maintype' : 'image', 'subtype' : 'jpg', 'filename' : 'res/email_sending/test.jpg'}, # 이미지 첨부파일
-    'audio' : {'maintype' : 'audio', 'subtype' : 'mp3', 'filename' : 'res/email_sending/test.mp3'}, # 오디오 첨부파일
-    'video' : {'maintype' : 'video', 'subtype' : 'mp4', 'filename' : 'res/email_sending/test.mp4'}, # 비디오 첨부파일
-    'application' : {'maintype' : 'application', 'subtype' : 'octect-stream', 'filename' : 'res/email_sending/test.pdf'}, # 그 외 첨부파일
-}
+def send_mail(smtp_info, msg):
+    with smtplib.SMTP(smtp_info['smtp_server'], smtp_info['smtp_port']) as server:
+        # TLS 보안 연결
+        server.starttls()
+        # 로그인
+        server.login(smtp_info['smtp_user_id'], smtp_info['smtp_user_pw'])
+        # 로그인 된 서버에 이메일 전송
+        response = server.sendmail(msg['from'], msg['to'], msg.as_string())
+        # 메시지를 보낼 때는 .as_string() 메서드를 사용해서 문자열로 바꿔준다.
+
+        # 이메일을 성공적으로 보내면 결과는 {}
+        if not response:
+            print('이메일을 성공적으로 보냈습니다.')
+        else:
+            print(response)
 
 def make_multimsg(msg_dict):
     multi = MIMEMultipart(_subtype='mixed')
 
-    for key, value in dict.items():
-        # 각 타입에 적절한 MIME.xxx() 함수를 호출하여 msg 객체를 생성한다.
+    for key, value in msg_dict.items():
+        # 각 타입에 적절한 MIMExxx() 함수를 호출하여 msg 객체를 생성한다.
         if key == 'text':
             with open(value['filename'], encoding='utf-8') as fp:
                 msg = MIMEText(fp.read(), _subtype=value['subtype'])
