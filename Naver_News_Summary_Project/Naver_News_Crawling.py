6ff4127ac579fbf405e1331687ebae729f1366d4
Daniel_Notebook <clwnssla1@bible.ac.kr>
2023-04-03 14:59:28 +0900

    fix: lis3 class_ type06_headline -> cluster

---
+++ Naver_News_Summary_Project/Naver_News_Crawling.py
@@ -0,0 +1,41 @@
+import requests
+from bs4 import BeautifulSoup
+import bs4.element
+import datetime
+
+# BeautifulSoup 객체 생성
+def get_soup_obj(url):
+    headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36'}
+    res = requests.get(url, headers = headers)
+    soup = BeautifulSoup(res.text, 'lxml')
+
+    return soup
+
+
+# 뉴스의 기본 정보 가져오괴
+def get_top3_news_info(sec, sid):
+    # 임시 이미지
+    default_img = 'https://search.naver.com/search.naver?where=image&sm=tab_niv&query=naver#'
+    # 해당 분야 상위 뉴스 목록 주소
+    sec_url = 'https://news.naver.com/main/main.naver?mode=LSD&mid=shm' \
+        + '&sid1=' \
+        + sid
+    print('section url : ', sec_url)
+
+    # 해당 분야 상위 뉴스 HTML 가져오기
+    soup = get_soup_obj(sec_url)
+
+    # 해당 분야 상위 뉴스 세 개 가져오기
+    news_list3 = []
+    lis3 = soup.find('ul', class_='cluster').find_all('li', limit=3)
+    for li in lis3:
+        # title : 뉴스 제목, news_url : 뉴스 URL, image_url : 이미지 URL
+        news_info = {
+            'title' : li.img.attrs.get('alt') if li.img else li.a.text.replace('\n', '').replace('\t', '').replace('\r', '') ,
+            'date' : li.find(class_='date').text,
+            'news_url' : li.a.attrs.get('href'),
+            'image_url' : li.img.attrs.get('src') if li.img else default_img
+        }
+        news_list3.append(news_info)
+    
+    return news_list3
