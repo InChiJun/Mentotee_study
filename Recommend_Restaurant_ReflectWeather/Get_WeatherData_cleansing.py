bfdae1ca4355bc46b9396042d3dd8d1fc9f28688
Daniel_Notebook <clwnssla1@bible.ac.kr>
2023-04-06 09:54:04 +0900

    Feat: create chapter9 code.

--- Recommend_Restaurant_ReflectWeather/Get_WeatherData_cleansing.py
+++ Recommend_Restaurant_ReflectWeather/Get_WeatherData_cleansing.py
@@ -6,7 +6,7 @@ village_weather_url = 'http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/
 
 service_key = 'VIqxEFRYT4zZpNNVDiZ7GWJMfTw3h%2FVmKCkQ7ivS8G%2BOfGlxzwl6Mb65rq70tqMRbR918sDVhGOuxTTSTYY8gw%3D%3D'
 base_date = datetime.datetime.today().strftime('%Y%m%d') # '20230405' == 기준 날짜
-base_time = '0800' # 날씨 값
+base_time = '0800' # 기준 시간
 nx = '59'
 ny = '126'
 
@@ -17,10 +17,20 @@ payload = 'serviceKey=' + service_key + '&' +\
     'nx=' + nx + '&' +\
     'ny=' + ny
 
+pty_code = {'0': '없음', '1': '비', '2': '비/눈', '3': '눈', '4': '소나기', '5': '빗방울', '6': '빗방울/눈날림', '/': '눈날림'}
+
+data = dict()
+data['data'] = base_date
+weather = dict()
+
 # 값 요청
 res = requests.get(village_weather_url + payload)
 try:
     items = res.json().get('response').get('body').get('items')
+    for item in items['item']:
+        # 기온
+        if item['category'] == 'TMP':
+            weather['tmp']
     print(items)
 except:
     print('날씨 정보 요청 실패 : ', res.text)
