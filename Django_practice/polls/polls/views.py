18e06387ecbf4383950cd2f6b60e944b7bb512f3
Daniel_Notebook <clwnssla1@bible.ac.kr>
2023-05-02 21:54:57 +0900

    Feat: Write Django code.(chapter3)

--- Django_practice/polls/polls/views.py
+++ Django_practice/polls/polls/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
-
+from .models import Question
 from django.http import HttpResponse
+from django.template import loader
+
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    lastest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': lastest_question_list}
+    return render(request, 'polls/index.html', context)
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+def results(request, question_id):
+    response = "You're looking at question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
