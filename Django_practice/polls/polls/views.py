56b50be3dd0941f0eaa0be11ed61b7865e688b53
Daniel_computer <clwnssla1@bible.ac.kr>
2023-05-04 00:45:55 +0900

    Feat: Finish Django project(Chapter_3)

--- Django_practice/polls/polls/views.py
+++ Django_practice/polls/polls/views.py
@@ -24,7 +24,7 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        """Return the last five qublished questions."""
+        """Return the last five published questions."""
         return Question.objects.order_by('-pub_date')[:5]
 
 class DetailView(generic.DetailView):
