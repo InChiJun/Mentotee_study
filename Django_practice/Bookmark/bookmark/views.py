0a50c9055b19d5da1d59c3cad69f2e6ec9f2f364
BibleCom <clwnssla1@bible.ac.kr>
2023-05-04 11:53:11 +0900

    Feat: Write Django code.(chapter 4)

--- Django_practice/Bookmark/bookmark/views.py
+++ Django_practice/Bookmark/bookmark/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from .models import Bookmark
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView
+
+class BookmarkCreateView(CreateView):
+    model = Bookmark
+    fields = ['site_name', 'url']
+    success_url = reverse_lazy('list')
+    template_name_suffix = '_create'
 
 class BookmarkListView(ListView):
     model = Bookmark
