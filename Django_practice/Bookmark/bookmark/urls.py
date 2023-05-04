5c0899b25cdacea3586d013d8b1c9408574d52f3
Daniel_Notebook <clwnssla1@bible.ac.kr>
2023-05-04 11:31:22 +0900

    Feat: Write Django code.(chapter4)

--- Django_practice/Bookmark/bookmark/urls.py
+++ Django_practice/Bookmark/bookmark/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import BookmarkListView
 
 urlpatterns = [
-    path('', BookmarkListView.as_view(), name='List'),
+    path('', BookmarkListView.as_view(), name='list'),
 ]
