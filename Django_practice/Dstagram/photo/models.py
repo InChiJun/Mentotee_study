5a9e3aa2a94f48aa15262c183a5605d171d1fa51
BibleCom <clwnssla1@bible.ac.kr>
2023-05-15 17:44:45 +0900

    Feat: Write django chapter_5 code.

--- Django_practice/Dstagram/photo/models.py
+++ Django_practice/Dstagram/photo/models.py
@@ -12,3 +12,9 @@ class Photo(models.Model):
 
     class Meta:
         ordering = ['-updated']
+
+    def __str__(self): # 작성자의 이름과 글 작성일을 합친 문자열을 반환
+        return self.author.username + " " + self.created.strftime("%Y-%m-%d %H:%M:%S")
+
+    def get_absolute_url(self): # 객체의 상세 페이지 주소를 반환하는 메서드
+        return reverse('photo:photo_detail', args=[str(self.id)])
