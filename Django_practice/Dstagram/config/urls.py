353f3a25e4a8db38bc0f9eb5fa1f7c79064340e3
Daniel_computer <clwnssla1@bible.ac.kr>
2023-05-18 08:32:16 +0900

    Feat: Write Django code(chapter 5.)

--- Django_practice/Dstagram/config/urls.py
+++ Django_practice/Dstagram/config/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from django.urls import path
-from django.views.generic.detail import DetailView
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('photo.urls')),
 ]
